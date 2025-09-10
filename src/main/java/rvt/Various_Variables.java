25f111a259481fd0d125fa589d6e8fc390e6ffe4
Valeriia Motas <a240248vm@rvt.lv>
2025-09-10 03:12:01 -0700

    Various_Variables

---
+++ src/main/java/rvt/Various_Variables.java
@@ -0,0 +1,16 @@
+package rvt;
+
+public class Various_Variables {
+
+    public static void main(String[] args) {
+
+        int chiken = 9000;
+        double bacon = 0,1;
+        String tractor = "Zetor";
+        System.out.println("Chiken:" + "\n" + chiken);
+        System.out.println("Bacon (kg):" + "\n" + bacon);
+        System.out.println("Tractor:" + "\n" + tractor);
+
+        System.out.println("And finally, a summary:" + "\n" + chiken + "\n" + bacon + "\n" + tractor);
+    }
+}
