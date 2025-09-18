3351ee2727003b028ba69c9a9b0391d071cf5cb7
24DP2VMota <a240248vm@rvt.lv>
2025-09-18 06:09:27 +0000

    Grades and Points

---
+++ src/main/java/rvt/Grades.java
@@ -0,0 +1,32 @@
+package rvt;
+import java.util.Scanner;
+public class Grades {
+    public static void main(String[] args) {
+        // Your code goes here...
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give points [0-100]: ");
+
+        int points = Integer.valueOf(scanner.nextInt());
+
+        if(points < 0) {
+            System.out.println("Impossible!");
+        } else if (points <= 49) {
+            System.out.println("Failed.");
+        } else if (points <= 59) {
+            System.out.println("10");
+        } else if (points <= 69) {
+            System.out.println("2");
+        } else if (points <= 79) {
+            System.out.println("3");
+        } else if (points <= 89) {
+            System.out.println("4");
+        } else if (points <=100) {
+            System.out.println("5");
+        } else {
+            System.out.println("Incredible!");
+        }
+        scanner.close();
+
+    }
+}
