8f29a8293a58625a8cd9d628f5075bcf234cdd45
24DP2VMota <a240248vm@rvt.lv>
2025-10-08 09:24:06 +0000

    From parameter to one

---
+++ src/main/java/rvt/From_parameter_to_one.java
@@ -0,0 +1,14 @@
+package rvt;
+
+public class From_parameter_to_one {
+    public static void main(String[] args) {
+        printFromNumberToOne(5);
+    }
+    
+    public static void printFromNumberToOne(int number) {
+        for(int i = number; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+    
+}
